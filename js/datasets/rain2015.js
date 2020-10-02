c7d8694c093ac3e85be91a4ef066f74f9a1649dc
kaisadilla <elveszett42@gmail.com>
2020-10-02 15:32:56 +0200

    s

---
+++ js/datasets/rain2015.js
@@ -0,0 +1,58 @@
+let rain2015 = {
+    "albacete":     307.5,
+    "alicante":     241.2,
+    "almeria":      254.5,
+    "alava":        787.5,
+    "asturias":     1021.6,
+    "avila":        262.0,
+    "badajoz":      309.3,
+    "baleares":     452.6, // Palma
+    "barcelona":    323.9,
+    "vizcaya":      1163.6,
+    "burgos":       383.2,
+    "caceres":      315.0,
+    "cadiz":        464.0,
+    "cantabria":    1095.6,
+    "castellon":    453.4,
+    "ciudad_real":  265.6,
+    "cordoba":      300.4,
+    "coruna":       1214.8, // Santiago de Compostela
+    "cuenca":       322.4,
+    "guipuzcoa":    1437.1,
+    "girona":       411.2,
+    "granada":      262.5,
+    "guadalajara":  272.4, // 2012
+    "huelva":       366.2,
+    "huesca":       406.9,
+    "jaen":         278.2,
+    "leon":         430.1,
+    "lleida":       248.8,
+    "lugo":         752.2,
+    "madrid":       278.4,
+    "malaga":       322.3,
+    "murcia":       236.5,
+    "navarra":      810.0,
+    "ourense":      495.6,
+    "palencia":     489.0, // 1981-2010 AEMET
+    "gran_canaria": 162.8, // Gando
+    "pontevedra":   1057.4,
+    "rioja":        435.2,
+    "salamanca":    219.4,
+    "tenerife":     213.2, // Santa Cruz
+    "segovia":      327.0,
+    "sevilla":      336.2,
+    "soria":        460.0,
+    "tarragona":    446.5,
+    "teruel":       397.6,
+    "toledo":       228.6,
+    "valencia":     420.0,
+    "valladolid":   349.8,
+    "zamora":       318.2,
+    "zaragoza":     336.5,
+    "ceuta":        408.0,
+    "melilla":      341.7,
+}
+
+/* source: INE 2015
+ * https://www.ine.es/daco/daco42/bme/c19.pdf
+ */
--- js/map_generation.js
+++ js/map_generation.js
@@ -13,24 +13,8 @@ function generate (dataset) {
     if (dataset === "unemployment") generateUnemployment();
     if (dataset === "diff_ocupacion") generateDiffOcupacion();
     if (dataset === "life_expectancy") generateLifeExpectancy();
-}
-
-function alterStyle() {
-    function styleb (feature) {
-        return {
-            weight: 1,
-            opacity: 1,
-            color: "white",
-            dashArray: "1",
-            fillOpacity: 1,
-            fillColor: "red"
-        }
-    }
-
-    geojson = L.geoJson(provincesData, {
-        style: styleb,
-        onEachFeature: onEachFeature
-    }).addTo(map);
+    if (dataset === "average_salary") generateAverageSalary();
+    if (dataset === "rain") generateRain();
 }
 
 function generateMap() {
@@ -42,6 +26,28 @@ function generateMap() {
         style: style,
         onEachFeature: onEachFeature
     }).addTo(map);
+
+    info = L.control();
+
+    info.onAdd = function(_) {
+        this.div = L.DomUtil.create("div", "info");
+        this.update();
+        return this.div;
+    }
+}
+
+function selectStyle(dataset) {
+
+    style = function (feature) {
+        return {
+            weight: 1,
+            opacity: 1,
+            color: "white",
+            dashArray: "1",
+            fillOpacity: 1,
+            fillColor: getColor(dataset[feature.properties.id])
+        }
+    }
 }
 
 function generatePopulation() {
@@ -56,26 +62,9 @@ function generatePopulation() {
         else                      return "#800026";
     }
 
-    style = function (feature) {
-        return {
-            weight: 1,
-            opacity: 1,
-            color: "white",
-            dashArray: "1",
-            fillOpacity: 1,
-            fillColor: getColor(population[feature.properties.id])
-        }
-    }
+    selectStyle(population);
 
     generateMap();
-
-    info = L.control();
-
-    info.onAdd = function(_) {
-        this.div = L.DomUtil.create("div", "info");
-        this.update();
-        return this.div;
-    }
 
     info.update = function(props) {
         if (props) {
@@ -130,26 +119,9 @@ function generateUnemployment() {
         else                 return "#800026";
     }
 
-    style = function (feature) {
-        return {
-            weight: 1,
-            opacity: 1,
-            color: "white",
-            dashArray: "1",
-            fillOpacity: 1,
-            fillColor: getColor(paro2020T2[feature.properties.id])
-        }
-    }
+    selectStyle(paro2020T2);
 
     generateMap();
-
-    info = L.control();
-
-    info.onAdd = function(_) {
-        this.div = L.DomUtil.create("div", "info");
-        this.update();
-        return this.div;
-    }
 
     info.update = function(props) {
         if (props) {
@@ -161,7 +133,7 @@ function generateUnemployment() {
         }
         else {
             this.div.innerHTML = `
-            <h4>Población por provincia</h4>
+            <h4>Desempleo por provincia</h4>
             Coloca el cursor sobre una provincia.
             `
         }
@@ -175,7 +147,7 @@ function generateUnemployment() {
         let div = L.DomUtil.create("div", "info legend");
 
         div.innerHTML += `
-            <b>Habitantes</b><br />
+            <b>% de desempleo</b><br />
             <i style = "background: ${getColor(0)}"></i> < 7% <br />
             <i style = "background: ${getColor(7)}"></i> 7% - 9% <br />
             <i style = "background: ${getColor(9)}"></i> 9% - 12% <br />
@@ -205,26 +177,9 @@ function generateDiffOcupacion() {
         else                   return "#800026";
     }
 
-    style = function (feature) {
-        return {
-            weight: 1,
-            opacity: 1,
-            color: "white",
-            dashArray: "1",
-            fillOpacity: 1,
-            fillColor: getColor(diffOcupacion2020T2[feature.properties.id])
-        }
-    }
+    selectStyle(diffOcupacion2020T2);
 
     generateMap();
-
-    info = L.control();
-
-    info.onAdd = function(_) {
-        this.div = L.DomUtil.create("div", "info");
-        this.update();
-        return this.div;
-    }
 
     info.update = function(props) {
         if (props) {
@@ -236,7 +191,7 @@ function generateDiffOcupacion() {
         }
         else {
             this.div.innerHTML = `
-            <h4>Población por provincia</h4>
+            <h4>Desigualdad de empleo</h4>
             Coloca el cursor sobre una provincia.
             `
         }
@@ -250,7 +205,7 @@ function generateDiffOcupacion() {
         let div = L.DomUtil.create("div", "info legend");
 
         div.innerHTML += `
-            <b>Habitantes</b><br />
+            <b>% de mujeres empleadas respecto de hombres</b><br />
             <i style = "background: ${getColor(1)}"></i> > 100% <br />
             <i style = "background: ${getColor(0.90)}"></i> 100% - 90% <br />
             <i style = "background: ${getColor(0.85)}"></i> 90% - 85% <br />
@@ -279,26 +234,9 @@ function generateLifeExpectancy() {
         else                   return "#800026";
     }
 
-    style = function (feature) {
-        return {
-            weight: 1,
-            opacity: 1,
-            color: "white",
-            dashArray: "1",
-            fillOpacity: 1,
-            fillColor: getColor(lifeExpectancy2019[feature.properties.id])
-        }
-    }
+    selectStyle(lifeExpectancy2019);
 
     generateMap();
-
-    info = L.control();
-
-    info.onAdd = function(_) {
-        this.div = L.DomUtil.create("div", "info");
-        this.update();
-        return this.div;
-    }
 
     info.update = function(props) {
         if (props) {
@@ -333,6 +271,121 @@ function generateLifeExpectancy() {
             <i style = "background: ${getColor(82)}"></i> 82 - 82.5 <br />
             <i style = "background: ${getColor(81)}"></i> 81 - 82 <br />
             <i style = "background: ${getColor(80)}"></i> < 81 <br />
+        `;
+
+        return div;
+    }
+
+    legend.addTo(map);
+}
+
+function generateAverageSalary() {
+    getColor = function (val) {
+        if      (isNaN(val))    return "transparent";
+        if      (val < 13_000)  return "#eaff6b";
+        else if (val < 15_000)  return "#c9e75c";
+        else if (val < 17_000)  return "#a9cf4e";
+        else if (val < 18_000)  return "#8ab740";
+        else if (val < 19_000)  return "#6ca033";
+        else if (val < 21_000)  return "#508926";
+        else if (val < 23_000)  return "#347219";
+        else                    return "#155c0c";
+    }
+
+    selectStyle(avgSalary2017);
+
+    generateMap();
+
+    info.update = function(props) {
+        if (props) {
+            this.div.innerHTML = `
+            <h4>Salario medio</h4>
+            <strong>${props.name}</strong> <br />
+            €${avgSalary2017[props.id].toLocaleString("en")}
+            `
+        }
+        else {
+            this.div.innerHTML = `
+            <h4>Salario medio</h4>
+            Coloca el cursor sobre una provincia.
+            `
+        }
+    }
+
+    info.addTo(map);
+
+    legend = L.control({position: "bottomright"});
+
+    legend.onAdd = function (_) {
+        let div = L.DomUtil.create("div", "info legend");
+
+        div.innerHTML += `
+            <b>Salario medio</b><br />
+            <i style = "background: ${getColor(0)}"></i> < €13 k <br />
+            <i style = "background: ${getColor(13_000)}"></i> €13 k - €15 k <br />
+            <i style = "background: ${getColor(15_000)}"></i> €15 k - €17 k <br />
+            <i style = "background: ${getColor(17_000)}"></i> €17 k - €18 k <br />
+            <i style = "background: ${getColor(18_000)}"></i> €18 k - €19 k <br />
+            <i style = "background: ${getColor(19_000)}"></i> €19 k - €21 k <br />
+            <i style = "background: ${getColor(21_000)}"></i> €21 k - €23 k <br />
+            <i style = "background: ${getColor(23_000)}"></i> < €23 k <br />
+        `;
+
+        return div;
+    }
+
+    legend.addTo(map);
+}
+
+function generateRain() {
+    getColor = function (val) {
+        if      (val < 250)  return "#e4ffff";
+        else if (val < 300)  return "#cedee0";
+        else if (val < 400)  return "#a9bec2";
+        else if (val < 500)  return "#869fa5";
+        else if (val < 700)  return "#64808a";
+        else if (val < 900)  return "#446370";
+        else if (val < 1100) return "#244758";
+        else                 return "#002c40";
+    }
+
+    selectStyle(rain2015);
+
+    generateMap();
+
+    info.update = function(props) {
+        if (props) {
+            this.div.innerHTML = `
+            <h4>Precipitación anual</h4>
+            <strong>${props.name}</strong> <br />
+            ${rain2015[props.id].toLocaleString("en")} mm
+            `
+        }
+        else {
+            this.div.innerHTML = `
+            <h4>Precipitación anual</h4>
+            Coloca el cursor sobre una provincia.
+            `
+        }
+    }
+
+    info.addTo(map);
+
+    legend = L.control({position: "bottomright"});
+
+    legend.onAdd = function (_) {
+        let div = L.DomUtil.create("div", "info legend");
+
+        div.innerHTML += `
+            <b>Salario medio</b><br />
+            <i style = "background: ${getColor(0)}"></i> < 200 mm <br />
+            <i style = "background: ${getColor(250)}"></i> 250 - 300 mm <br />
+            <i style = "background: ${getColor(300)}"></i> 300 - 400 mm <br />
+            <i style = "background: ${getColor(400)}"></i> 400 - 500 mm <br />
+            <i style = "background: ${getColor(500)}"></i> 500 - 700 mm <br />
+            <i style = "background: ${getColor(700)}"></i> 700 - 900 mm <br />
+            <i style = "background: ${getColor(900)}"></i> 900 - 1100 mm <br />
+            <i style = "background: ${getColor(1100)}"></i> > 1100 mm <br />
         `;
 
         return div;
