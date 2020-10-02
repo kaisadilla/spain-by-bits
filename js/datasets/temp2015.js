2e0b5ec7824cee2e514c112d7bbdd506c778d8ff
kaisadilla <58343029+kaisadilla@users.noreply.github.com>
2020-10-02 18:40:20 +0200

    Some palletes changed.

---
+++ js/datasets/temp2015.js
@@ -0,0 +1,58 @@
+let temp2015 = {
+    "albacete":     15.5,
+    "alicante":     19.1,
+    "almeria":      19.9,
+    "alava":        12.4,
+    "asturias":     13.8,
+    "avila":        12.6,
+    "badajoz":      18.2,
+    "baleares":     19.0, // Palma
+    "barcelona":    17.5,
+    "vizcaya":      15.2,
+    "burgos":       11.2,
+    "caceres":      17.1,
+    "cadiz":        19.1,
+    "cantabria":    15.1,
+    "castellon":    18.5,
+    "ciudad_real":  16.7,
+    "cordoba":      19.1,
+    "coruna":       14.1, // Santiago de Compostela
+    "cuenca":       14.9,
+    "guipuzcoa":    14.1,
+    "girona":       15.8,
+    "granada":      16.6,
+    "guadalajara":  13.6, // 2012
+    "huelva":       19.0,
+    "huesca":       15.5,
+    "jaen":         18.2,
+    "leon":         11.4,
+    "lleida":       16.0,
+    "lugo":         13.1,
+    "madrid":       16.6,
+    "malaga":       19.8,
+    "murcia":       20.0,
+    "navarra":      14.0, // 2014
+    "ourense":      15.9,
+    "palencia":     11.6, // 1981-2010 AEMET
+    "gran_canaria": 21.9, // Gando
+    "pontevedra":   15.1,
+    "rioja":        14.5,
+    "salamanca":    12.7,
+    "tenerife":     22.1, // Santa Cruz
+    "segovia":      13.4,
+    "sevilla":      20.0,
+    "soria":        11.7,
+    "tarragona":    18.5,
+    "teruel":       13.5,
+    "toledo":       17.0,
+    "valencia":     19.2,
+    "valladolid":   13.4,
+    "zamora":       13.8,
+    "zaragoza":     16.6,
+    "ceuta":        19.3,
+    "melilla":      19.6,
+}
+
+/* source: INE 2015
+ * https://www.ine.es/daco/daco42/bme/c19.pdf
+ */
--- js/map_generation.js
+++ js/map_generation.js
@@ -8,6 +8,23 @@ let legend;
 let getColor;
 let style;
 
+function moveIslands() {
+    console.log("owo");
+    move(findById("gran_canaria"));
+    move(findById("tenerife"));
+    
+    function move (island) {
+        for (let i = 0; i < island.geometry.coordinates.length; i++) {
+            for (let j = 0; j < island.geometry.coordinates[i].length; j++) {
+                for (let k = 0; k < island.geometry.coordinates[i][j].length; k++) {
+                    island.geometry.coordinates[i][j][k][0] += 5;
+                    island.geometry.coordinates[i][j][k][1] += 9;
+                }
+            }
+        }
+    }
+}
+
 function generate (dataset) {
     if (dataset === "population") generatePopulation();
     if (dataset === "unemployment") generateUnemployment();
@@ -15,6 +32,16 @@ function generate (dataset) {
     if (dataset === "life_expectancy") generateLifeExpectancy();
     if (dataset === "average_salary") generateAverageSalary();
     if (dataset === "rain") generateRain();
+    if (dataset === "temperature") generateTemperature();
+}
+
+function findById (id) {
+    for (let i = 0; i < provincesData.features.length; i++) {
+        if (provincesData.features[i].properties.id === id) {
+            return provincesData.features[i];
+        }
+    }
+    return null;
 }
 
 function generateMap() {
@@ -36,8 +63,7 @@ function generateMap() {
     }
 }
 
-function selectStyle(dataset) {
-
+function setStyleSource(dataset) {
     style = function (feature) {
         return {
             weight: 1,
@@ -51,18 +77,18 @@ function selectStyle(dataset) {
 }
 
 function generatePopulation() {
-    getColor = function (val) {
-        if      (val < 50_000)    return "#ffeda0";
-        else if (val < 100_000)   return "#fed974";
-        else if (val < 200_000)   return "#feb24c";
-        else if (val < 500_000)   return "#fd8d3c";
-        else if (val < 1_000_000) return "#fc4e2a";
-        else if (val < 2_000_000) return "#e31a1c";
-        else if (val < 5_000_000) return "#bd0026";
-        else                      return "#800026";
-    }
+    setStyleSource(population);
 
-    selectStyle(population);
+    getColor = function (val) {
+        if      (val < 100_000)   return "#ede8ff";
+        else if (val < 200_000)   return "#e1d9fb";
+        else if (val < 400_000)   return "#d5cbf7";
+        else if (val < 800_000)   return "#b9a8ec";
+        else if (val < 1_600_000) return "#9f86df";
+        else if (val < 3_200_000) return "#8563d2";
+        else if (val < 5_000_000) return "#6b3fc3";
+        else                      return "#4f12b3";
+    }
 
     generateMap();
 
@@ -91,13 +117,13 @@ function generatePopulation() {
 
         div.innerHTML += `
             <b>Habitantes</b><br />
-            <i style = "background: ${getColor(0)}"></i> &lt; 50 k <br />
-            <i style = "background: ${getColor(50_000)}"></i> &lt; 100 k <br />
-            <i style = "background: ${getColor(100_000)}"></i> &lt; 200 k <br />
-            <i style = "background: ${getColor(200_000)}"></i> &lt; 500 k <br />
-            <i style = "background: ${getColor(500_000)}"></i> &lt; 1 m <br />
-            <i style = "background: ${getColor(1_000_000)}"></i> &lt; 2 m <br />
-            <i style = "background: ${getColor(2_000_000)}"></i> &lt; 5 m <br />
+            <i style = "background: ${getColor(0)        }"></i> < 100 k <br />
+            <i style = "background: ${getColor(100_000)  }"></i> < 200 k <br />
+            <i style = "background: ${getColor(200_000)  }"></i> < 400 k <br />
+            <i style = "background: ${getColor(400_000)  }"></i> < 800 k <br />
+            <i style = "background: ${getColor(800_000)  }"></i> < 1.6 m <br />
+            <i style = "background: ${getColor(1_600_000)}"></i> < 3.2 m <br />
+            <i style = "background: ${getColor(3_200_000)}"></i> < 5 m <br />
             <i style = "background: ${getColor(5_000_000)}"></i> 5+ m <br />
         `;
 
@@ -109,17 +135,17 @@ function generatePopulation() {
 
 function generateUnemployment() {
     getColor = function (val) {
-        if      (val < 7)    return "#ffeda0";
-        else if (val < 9)    return "#fed974";
-        else if (val < 12)   return "#feb24c";
-        else if (val < 15)   return "#fd8d3c";
-        else if (val < 18)   return "#fc4e2a";
-        else if (val < 21)   return "#e31a1c";
-        else if (val < 24)   return "#bd0026";
-        else                 return "#800026";
+        if      (val < 8)    return "#ffe8f1";
+        else if (val < 10)   return "#ebcfd9";
+        else if (val < 12)   return "#d8b5c2";
+        else if (val < 15)   return "#c59dab";
+        else if (val < 17)   return "#a87989";
+        else if (val < 19)   return "#8c5668";
+        else if (val < 22)   return "#703449";
+        else                 return "#54112b";
     }
 
-    selectStyle(paro2020T2);
+    setStyleSource(paro2020T2);
 
     generateMap();
 
@@ -148,14 +174,14 @@ function generateUnemployment() {
 
         div.innerHTML += `
             <b>% de desempleo</b><br />
-            <i style = "background: ${getColor(0)}"></i> < 7% <br />
-            <i style = "background: ${getColor(7)}"></i> 7% - 9% <br />
-            <i style = "background: ${getColor(9)}"></i> 9% - 12% <br />
-            <i style = "background: ${getColor(12)}"></i>  12% - 15% <br />
-            <i style = "background: ${getColor(15)}"></i>  15% - 18% <br />
-            <i style = "background: ${getColor(18)}"></i>  18% - 21% <br />
-            <i style = "background: ${getColor(21)}"></i>  21% - 24% <br />
-            <i style = "background: ${getColor(24)}"></i> > 24% <br />
+            <i style = "background: ${getColor(0) }"></i> < 8 % <br />
+            <i style = "background: ${getColor(8) }"></i> < 10 % <br />
+            <i style = "background: ${getColor(10)}"></i> < 12 % <br />
+            <i style = "background: ${getColor(12)}"></i> < 15 % <br />
+            <i style = "background: ${getColor(15)}"></i> < 17 % <br />
+            <i style = "background: ${getColor(17)}"></i> < 19 % <br />
+            <i style = "background: ${getColor(19)}"></i> < 22 % <br />
+            <i style = "background: ${getColor(22)}"></i> 22+ % <br />
         `;
 
         return div;
@@ -177,7 +203,7 @@ function generateDiffOcupacion() {
         else                   return "#800026";
     }
 
-    selectStyle(diffOcupacion2020T2);
+    setStyleSource(diffOcupacion2020T2);
 
     generateMap();
 
@@ -224,17 +250,17 @@ function generateDiffOcupacion() {
 
 function generateLifeExpectancy() {
     getColor = function (val) {
-        if      (val >= 84.5)  return "#ffeda0";
-        else if (val >= 84)    return "#fed974";
-        else if (val >= 83.5)  return "#feb24c";
-        else if (val >= 83)    return "#fd8d3c";
-        else if (val >= 82.5)  return "#fc4e2a";
-        else if (val >= 82)    return "#e31a1c";
-        else if (val >= 81)    return "#bd0026";
-        else                   return "#800026";
+        if      (val < 81.0) return "#e9ffd4";
+        else if (val < 82.0) return "#d3ebbc";
+        else if (val < 82.5) return "#bdd7a5";
+        else if (val < 83.0) return "#a7c38e";
+        else if (val < 83.5) return "#87a66d";
+        else if (val < 84.0) return "#678a4d";
+        else if (val < 84.5) return "#496e2e";
+        else                 return "#2a540d";
     }
 
-    selectStyle(lifeExpectancy2019);
+    setStyleSource(lifeExpectancy2019);
 
     generateMap();
 
@@ -263,14 +289,14 @@ function generateLifeExpectancy() {
 
         div.innerHTML += `
             <b>Habitantes</b><br />
-            <i style = "background: ${getColor(84.5)}"></i> > 84.5 <br />
-            <i style = "background: ${getColor(84)}"></i> 84 - 84.5 <br />
-            <i style = "background: ${getColor(83.5)}"></i> 83.5 - 84 <br />
-            <i style = "background: ${getColor(83)}"></i> 83 - 83.5 <br />
-            <i style = "background: ${getColor(82.5)}"></i> 82.5 - 83 <br />
-            <i style = "background: ${getColor(82)}"></i> 82 - 82.5 <br />
-            <i style = "background: ${getColor(81)}"></i> 81 - 82 <br />
-            <i style = "background: ${getColor(80)}"></i> < 81 <br />
+            <i style = "background: ${getColor(0)   }"></i> < 81 <br />
+            <i style = "background: ${getColor(81.0)}"></i> < 82 <br />
+            <i style = "background: ${getColor(82.0)}"></i> < 82.5 <br />
+            <i style = "background: ${getColor(82.5)}"></i> < 83 <br />
+            <i style = "background: ${getColor(83.0)}"></i> < 83.5 <br />
+            <i style = "background: ${getColor(83.5)}"></i> < 84 <br />
+            <i style = "background: ${getColor(84.0)}"></i> < 84.5 <br />
+            <i style = "background: ${getColor(84.5)}"></i> 84.5+ <br />
         `;
 
         return div;
@@ -282,17 +308,17 @@ function generateLifeExpectancy() {
 function generateAverageSalary() {
     getColor = function (val) {
         if      (isNaN(val))    return "transparent";
-        if      (val < 13_000)  return "#eaff6b";
-        else if (val < 15_000)  return "#c9e75c";
-        else if (val < 17_000)  return "#a9cf4e";
-        else if (val < 18_000)  return "#8ab740";
-        else if (val < 19_000)  return "#6ca033";
-        else if (val < 21_000)  return "#508926";
-        else if (val < 23_000)  return "#347219";
-        else                    return "#155c0c";
+        if      (val < 13_000)  return "#e6ffe8";
+        else if (val < 15_000)  return "#ceeecf";
+        else if (val < 17_000)  return "#b6deb7";
+        else if (val < 18_000)  return "#9fcd9e";
+        else if (val < 19_000)  return "#7db47a";
+        else if (val < 21_000)  return "#5b9b56";
+        else if (val < 23_000)  return "#398332";
+        else                    return "#086b06";
     }
 
-    selectStyle(avgSalary2017);
+    setStyleSource(avgSalary2017);
 
     generateMap();
 
@@ -339,7 +365,7 @@ function generateAverageSalary() {
 
 function generateRain() {
     getColor = function (val) {
-        if      (val < 250)  return "#e4ffff";
+        if      (val < 250)  return "#e8efef";
         else if (val < 300)  return "#cedee0";
         else if (val < 400)  return "#a9bec2";
         else if (val < 500)  return "#869fa5";
@@ -349,7 +375,7 @@ function generateRain() {
         else                 return "#002c40";
     }
 
-    selectStyle(rain2015);
+    setStyleSource(rain2015);
 
     generateMap();
 
@@ -386,6 +412,63 @@ function generateRain() {
             <i style = "background: ${getColor(700)}"></i> 700 - 900 mm <br />
             <i style = "background: ${getColor(900)}"></i> 900 - 1100 mm <br />
             <i style = "background: ${getColor(1100)}"></i> > 1100 mm <br />
+        `;
+
+        return div;
+    }
+
+    legend.addTo(map);
+}
+
+function generateTemperature() {
+    getColor = function (val) {
+        if      (val < 12.0)  return "#fff8ad";
+        else if (val < 13.5)  return "#fae791";
+        else if (val < 15.0)  return "#f7d476";
+        else if (val < 16.5)  return "#f4b24b";
+        else if (val < 18.0)  return "#f29830";
+        else if (val < 19.5)  return "#f07b18";
+        else if (val < 21.0)  return "#ee5a07";
+        else                  return "#eb2907";
+    }
+
+    setStyleSource(temp2015);
+
+    generateMap();
+
+    info.update = function(props) {
+        if (props) {
+            this.div.innerHTML = `
+            <h4>Temperatura promedio anual</h4>
+            <strong>${props.name}</strong> <br />
+            ${temp2015[props.id].toLocaleString("en")} °C
+            `
+        }
+        else {
+            this.div.innerHTML = `
+            <h4>Temperatura promedio anual</h4>
+            Coloca el cursor sobre una provincia.
+            `
+        }
+    }
+
+    info.addTo(map);
+
+    legend = L.control({position: "bottomright"});
+
+    legend.onAdd = function (_) {
+        let div = L.DomUtil.create("div", "info legend");
+
+        div.innerHTML += `
+            <b>Temperatura</b><br />
+            <i style = "background: ${getColor(0)}"></i> < 12 °C <br />
+            <i style = "background: ${getColor(12.0)}"></i> < 13.5 °C <br />
+            <i style = "background: ${getColor(13.5)}"></i> < 15 °C <br />
+            <i style = "background: ${getColor(15.0)}"></i> < 16.5 °C <br />
+            <i style = "background: ${getColor(16.5)}"></i> < 18 °C <br />
+            <i style = "background: ${getColor(18.0)}"></i> < 19.5 °C <br />
+            <i style = "background: ${getColor(19.5)}"></i> < 21 °C <br />
+            <i style = "background: ${getColor(21.0)}"></i> 21+ °C <br />
         `;
 
         return div;
